e6806aa7cd0d6ab98afe48bc6315140a09c1c903
Kwstas Chervatidis <kostasxerv@gmail.com>
2019-09-03 12:24:17 +0300

    add win animations + text

--- src/wheelOfFortune.js
+++ src/wheelOfFortune.js
@@ -1,11 +1,11 @@
 // Wheel Of Fortune scene
-const { Sprite, Container, PixiText } = window.px
+const { Sprite, Container, AnimatedSprite, BitmapText, randInt, newArray } = window.px
 console.warn('WheelOfFortune loaded')
 class WheelOfFortune extends Container {
   // constructor
   constructor () {
     super()
-    this.load = PIXI.loader
+    this.loader = PIXI.loader
 
     this.rotationTime = 5
     this.slicePrizes = [20, 500, 30, 300, 40, 200, 50, 100, 10, 1000]
@@ -24,63 +24,56 @@ class WheelOfFortune extends Container {
     // loading assets
     const resources = [{
       key: 'wheel',
-      source: 'http://10.0.0.83:3000/dev/wheel-of-fortune/spin-wheel.png'
-    }, {
-      key: 'central-frame',
-      source: 'http://10.0.0.83:3000/dev/wheel-of-fortune/central-frame.png'
-    }, {
-      key: 'pin',
-      source: 'http://10.0.0.83:3000/dev/wheel-of-fortune/pointer.png'
+      source: 'http://10.0.0.83:3000/dev/wheel-of-fortune/wheel.json'
     }, {
       key: 'background',
       source: 'http://10.0.0.83:3000/dev/wheel-of-fortune/background.jpg'
     }, {
-      key: 'wheel-shade',
-      source: 'http://10.0.0.83:3000/dev/wheel-of-fortune/wheel-shade.png'
+      key: 'you-win',
+      source: 'http://10.0.0.83:3000/dev/wheel-of-fortune/animation/you-win.json'
     }, {
-      key: 'win-highlight',
-      source: 'http://10.0.0.83:3000/dev/wheel-of-fortune/win-highlight.png'
+      key: 'panel-animation',
+      source: 'http://10.0.0.83:3000/dev/wheel-of-fortune/animation/panel-animation.json'
+    }, {
+      key: 'coin-flip',
+      source: 'http://10.0.0.83:3000/dev/wheel-of-fortune/animation/coin-flip.json'
+    }, {
+      key: 'wof-font',
+      source: 'http://10.0.0.83:3000/dev/wheel-of-fortune/wof-font.xml'
     }]
 
     resources.forEach(r => {
-      if (!this.load.resources[r.key]) {
-        this.load.add(r.key, r.source)
+      if (!this.loader.resources[r.key]) {
+        this.loader.add(r.key, r.source)
       }
     })
 
-    this.load.load()
+    this.loader.load()
     if (typeof callback === 'function') {
-      this.load.onComplete.add(callback)
+      this.loader.onComplete.add(callback)
     }
   }
 
   // method to be executed once the scene has been created
   create () {
     this.addChild(Sprite.fromImage('background'))
-    const wheelShade = this.addChild(new Sprite('wheel-shade', 640, 380))
+    const wheelShade = this.addChild(new Sprite('wheel-shade.png', 640, 380, 1))
     wheelShade.anchor.set(0.5)
     // adding the wheel in the middle of the canvas
-    this.wheel = this.addChild(new Sprite('wheel', 640, 360))
+    this.wheel = this.addChild(new Sprite('spin-wheel.png', 640, 360, 2))
     this.wheel.anchor.set(0.5)
 
-    this.winHl = this.addChild(new Sprite('win-highlight'))
+    this.winHl = this.addChild(new Sprite('win-highlight.png', 0, 0, 3)).hide()
     this.winHl.x = 551 + this.winHl.width / 2
     this.winHl.y = 80 + this.winHl.height + 30
     this.winHl.pivot.y = this.winHl.height + 30
     this.winHl.pivot.x = this.winHl.width / 2
 
     // adding the pin in the middle of the canvas
-    this.pin = this.addChild(new Sprite('pin', 640, 35))
+    this.pin = this.addChild(new Sprite('pointer.png', 640, 35, 4))
     this.pin.anchor.set(0.5, 0)
 
-    // adding the text field
-    this.prizeText = this.addChild(new PixiText(1280 / 2, 690, 2, 'Spin the wheel', 200, {
-      fontSize: 32,
-      fill: 'white'
-    }))
-    // this.prizeText.anchor.set(0.5)
-
-    const centralFrame = this.addChild(new Sprite('central-frame'))
+    const centralFrame = this.addChild(new Sprite('central-frame.png', 0, 0, 4))
     centralFrame.x = 1280 / 2 - centralFrame.width / 2
     centralFrame.y = 720 / 2 - centralFrame.height / 2
 
@@ -89,21 +82,35 @@ class WheelOfFortune extends Container {
 
     // waiting for your input, then calling "spinWheel" function
     this.on('pointerdown', this.spinWheel, this)
+
+    // win animation
+
+    this.winAnimation = this.addChild(new AnimatedSprite(this.loader.resources['you-win'].spritesheet.animations['win-frame'], 640, 250, 5)).hide()
+    this.winAnimation.anchor.set(0.5)
+    this.winAnimation.animationSpeed = 0.3
+
+    this.panelAnimation = this.addChild(new AnimatedSprite(this.loader.resources['panel-animation'].spritesheet.animations['panel-frame'], 640, 460, 5, true)).hide()
+    this.panelAnimation.anchor.set(0.5)
+    this.panelAnimation.animationSpeed = 0.4
+    this.winText = this.addChild(new BitmapText('', { font: '120px wof-font' }, 640, 445, 5)).hide()
+
   }
 
   // function to spin the wheel
   spinWheel (deg) {
     // can we spin the wheel?
     if (!this.canSpin) return
+
     // now the wheel cannot spin because it's already spinning
     this.canSpin = false
+
     // hide hl image
-    this.winHl.visible = false
+    this.winHl.hide()
+    this.winAnimation.hide()
+    this.winText.setText('').hide()
+    this.panelAnimation.hide()
 
     if (isNaN(deg)) deg = null
-
-    // resetting text field
-    this.prizeText.text = 'Good Luck!'
 
     // the wheel will spin round from 2 to 4 times. This is just coreography
     // var rounds = Math.max(5, Math.floor(Math.random() * 8))
@@ -122,8 +129,8 @@ class WheelOfFortune extends Container {
     // add percent of cycle in rads to display correct result (this depends on the position of the marker)
     const rads = ((360 * rounds + degrees) * Math.PI / 180) + Math.PI + 0.314
     // before the wheel ends spinning, we already know the prize according to "degrees" rotation and the number of slices
-    var prize = this.slices - 1 - Math.floor(degrees / (360 / this.slices))
-
+    var prizeIdx = this.slices - 1 - Math.floor(degrees / (360 / this.slices))
+    this.prize = this.slicePrizes[prizeIdx]
     // reset the rads rotation of the wheel
     this.wheel.rotation = 0
     let edge = 1
@@ -140,8 +147,6 @@ class WheelOfFortune extends Container {
         }
       },
       onComplete: () => {
-        // displaying prize text
-        this.prizeText.text = this.slicePrizes[prize]
         // show the hl
         this.showWin()
         // player can spin again
@@ -180,7 +185,11 @@ class WheelOfFortune extends Container {
     }
 
     calcHlPosition()
-    setTimeout(() => (this.winHl.visible = true), 100)
+    setTimeout(() => (this.winHl.show()), 100)
+    this.winText.setText(this.prize.toFixed(2))
+    this.winText.show()
+    this.winAnimation.play()
+    this.panelAnimation.play()
   }
 }
 
