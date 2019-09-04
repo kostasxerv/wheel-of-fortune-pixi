1ece78cfcd2bbf21d9e93183209aafd7e5dd9ffb
Kwstas Chervatidis <kostasxerv@gmail.com>
2019-09-04 17:10:27 +0300

    helper functions  number (to deg, to rad)

---
+++ src/helpers.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-extend-native */
+Number.prototype.toRad = function () {
+  return this * Math.PI / 180
+}
+
+Number.prototype.toDeg = function () {
+  return this * 180 / Math.PI
+}
